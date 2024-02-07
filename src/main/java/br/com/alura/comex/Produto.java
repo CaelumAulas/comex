c95efb61c2c0f21da9255627eadae5532f0e9753
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-02-06 23:39:26 -0300

    finished exercise: 2) Crie a classe Produto

---
+++ src/main/java/br/com/alura/comex/Produto.java
@@ -0,0 +1,48 @@
+package br.com.alura.comex;
+
+public class Produto {
+
+    private String nome;
+    private String descricao;
+    private Double preco;
+    private int quantidade;
+
+    public Produto (String nome, String descricao, Double preco, int quantidade){
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
