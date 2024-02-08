e3cd8d626a2ba5eea8f184be19ca4817728c988c
Gabriel Borges <gabriel.borges.2002@hotmail.com>
2024-02-08 11:35:55 -0300

    tarefa 5

---
+++ src/main/java/br/com/alura/comex/Cliente.java
@@ -0,0 +1,10 @@
+package br.com.alura.comex;
+
+public class Cliente {
+    String nome;
+    String cpf;
+    String email;
+    String profissao;
+    String telefone;
+    Endereco endereco;
+}
--- src/main/java/br/com/alura/comex/Teste.java
+++ src/main/java/br/com/alura/comex/Teste.java
@@ -2,9 +2,19 @@ package br.com.alura.comex;
 
 public class Teste {
     public static void main(String[] args) {
-        Produto produto = new Produto();
-        produto.nome = "Livro";
-        produto.desc = "Conjunto de folhas impressas";
-        produto.show();
+        Endereco endereco = new Endereco();
+        endereco.bairro = "República";
+        endereco.cidade = "São Paulo";
+        endereco.complemento = "Casa";
+        endereco.estado = "SP";
+        endereco.rua = "Praça da República";
+        endereco.numero = 213;
+        Cliente cliente = new Cliente();
+        cliente.nome = "Luciano";
+        cliente.cpf = "456.736.540-22";
+        cliente.email = "luciano@gmail.com";
+        cliente.profissao = "Programador";
+        cliente.telefone = "(11) 98744-5443";
+        cliente.endereco = endereco;
     }
 }
