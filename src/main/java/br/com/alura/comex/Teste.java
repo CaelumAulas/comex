d3bc74c61be6a337b7afb21c3cc44077fc2aea58
Gabriel Borges <gabriel.borges.2002@hotmail.com>
2024-02-08 11:16:30 -0300

    tarefa 3

---
+++ src/main/java/br/com/alura/comex/Teste.java
@@ -0,0 +1,9 @@
+package br.com.alura.comex;
+
+public class Teste {
+    public static void main(String[] args) {
+        Produto produto1 = new Produto();
+        Produto produto2 = new Produto();
+        System.out.println(produto1.equals(produto2));
+    }
+}
