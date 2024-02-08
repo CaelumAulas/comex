85fe364ecbe138afbe9d657424f8a95cefa71c47
Gabriel Borges <gabriel.borges.2002@hotmail.com>
2024-02-08 11:39:26 -0300

    tarefa 6

--- src/main/java/br/com/alura/comex/Produto.java
+++ src/main/java/br/com/alura/comex/Produto.java
@@ -1,10 +1,42 @@
 package br.com.alura.comex;
 
 public class Produto {
-    String nome;
-    String desc;
-    float preco;
-    int qtd;
+    private String nome;
+    private String desc;
+    private float preco;
+    private int qtd;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public int getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
 
     public void show() {
         System.out.println(">> Dados do produto");
