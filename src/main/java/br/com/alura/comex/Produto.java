6c6e857530cb468f3d6731cd9047c86006042793
miller <mmillermonteiro@gmail.com>
2024-02-08 23:29:18 -0300

    Terminado a atividade 4) Crie um comportamento de impressão dos dados de um produto

--- src/main/java/br/com/alura/comex/Produto.java
+++ src/main/java/br/com/alura/comex/Produto.java
@@ -5,4 +5,45 @@ public class Produto{
     String descricao;
     double precoUnitario;
     int quantidade;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+
+    }
+
+    //Método para imprimir os dados do produto
+     void imprimirDados(){
+            System.out.println(">> Dados do produto");
+            System.out.println(":: Nome: " + getNome());
+            System.out.println(":: Descrição: " + getDescricao());
+        }
+
 }
--- src/main/java/br/com/alura/comex/Teste.java
+++ src/main/java/br/com/alura/comex/Teste.java
@@ -1,11 +1,12 @@
 package br.com.alura.comex;
 
-public class TesteComparacao {
+public class Teste {
     public static void main(String[] args) {
         Produto teste1 = new Produto();
         Produto teste2 = new Produto();
         boolean comparacao = teste1 == teste2;
-        System.out.println(comparacao);
+        teste1.setNome("Produto de teste");
+        teste1.setDescricao("Esse produto é um teste");
+        teste1.imprimirDados();
     }
-
 }
