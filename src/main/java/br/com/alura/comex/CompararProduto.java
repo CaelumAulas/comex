a116fd048280d2361be7dccc5e0413c9f098f1e4
vitorfuzo <vitorfuzo720@gmail.com>
2024-02-19 14:11:14 -0300

    Create CompararProduto class

---
+++ src/main/java/br/com/alura/comex/CompararProduto.java
@@ -0,0 +1,17 @@
+package br.com.alura.comex;
+
+public class CompararProduto {
+
+    public static void main(String[] args) {
+        //Criando dois objetos da classe Produto
+        Produto produto1 = new Produto("Camisa", "Camisa de algodão", 29.99f, 10);
+        Produto produto2 = new Produto("Calça", "Calça jeans", 49.99f, 5);
+
+        //Comparando as referências dos objetos
+        if (produto1 == produto2) {
+            System.out.println("Os produtos são iguais.");
+        } else {
+            System.out.println("Os produtos são diferentes.");
+        }
+    }
+}
