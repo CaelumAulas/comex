5d6c97a2868ca96092396c5706c8f71d74126a7c
thayrinerenata <thayrinerenata@gmail.com>
2024-02-20 14:52:17 -0300

    Sprint 7

--- src/main/entrega7/src/Main.java
+++ src/main/entrega7/src/Main.java
@@ -1,20 +1,17 @@
+import ST_Comex.produto;
+
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args){
+        produto vendinha = new produto();
+        vendinha.setNome("Freegells");
+        vendinha.setDescricao("Bala de morango");
+        vendinha.setPreco_unitario(2.50);
+        vendinha.quantidade = 10;
 
-            Scanner cobasi = new Scanner(System.in);
-
-            String nome1 = "Jardinagem";
-            String descricao1 = "Margarida branca";
-
-            String nome2 = "Mar";
-            String descricao2 = "Peixe beta azul";
-
-            System.out.println(">> Dados do produto");
-            System.out.println("::Nome: " + nome1);
-            System.out.println("::Descrição: " + descricao1);
-
-            System.out.println(">> Dados do produto");
-            System.out.println("::Nome: " + nome2);
-            System.out.println("::Descrição: " + descricao2);
+        System.out.println(">> Dados do ST_Comex.produto <<");
+        System.out.println("Nome: " + vendinha.getNome());
+        System.out.println("Descrição: " + vendinha.getDescricao());
+        System.out.println("Preço unitário: " + vendinha.getPreco_unitario());
+        System.out.println("Quantidade: " + vendinha.quantidade);
     }
 }
