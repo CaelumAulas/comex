6a566b103e1c369f51925164154111f2cd81aa83
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-03-13 18:01:12 -0300

    2) Crie uma classe para testar a integração com o ViaCEP

--- src/main/java/br/com/alura/comex/APIViaJava.java
+++ src/main/java/br/com/alura/comex/APIViaJava.java
@@ -1,16 +1,14 @@
 package br.com.alura.comex;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class APIViaCEP {
-    public static <ViaCepResponse> ViaCepResponse getAddress(String cep) throws IOException, InterruptedException {
+public class APIViaJava {
+    public static APIViaCepResponse getAddress(String cep) throws IOException, InterruptedException {
         String url = String.format("https://viacep.com.br/ws/%s/json/", cep);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
@@ -18,16 +16,13 @@ public class APIViaCEP {
         Gson gson = new Gson();
 
         if (response.statusCode() == 400) {
-            throw new RuntimeException("cep inválido");
+            throw new RuntimeException("CEP inválido");
         } else if (response.statusCode() == 404) {
-            throw new RuntimeException("url inválida");
+            throw new RuntimeException("URL inválida");
         } else if (response.statusCode() == 200) {
-            JsonObject viaCepResponse = gson.fromJson(response.body(), JsonObject.class);
-            if (viaCepResponse.getCep() == null) {
-                throw new RuntimeException("cep não encontrado");
-            }
-            return (ViaCepResponse) viaCepResponse;
+            APIViaCepResponse viaCepResponse = gson.fromJson(response.body(), APIViaCepResponse.class);
+            return viaCepResponse;
         }
-        throw new RuntimeException("erro desconhecido");
+        throw new RuntimeException("Erro desconhecido");
     }
 }
